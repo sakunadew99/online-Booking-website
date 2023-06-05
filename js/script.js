1a787b73448db60033b28889d77de4ab01d205c5
sakunadew99 <sakunadew99@gmail.com>
2023-06-05 13:43:54 +0530

    add swiper style link and imge slider

--- js/script.js
+++ js/script.js
@@ -2,3 +2,26 @@ let navbar = document.querySelector('.header .navbar');
 document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
 }
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+}
+
+var swiper = new Swiper(".home-slider", {
+    loop: true,
+    effect: "coverflow",
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "auto",
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+      },
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+  });
